0c2f1075ebc8871638be4806b2a2277508d35fbb
Raghu Betina <rbetina@gmail.com>
2013-06-05 08:16:32 -0500

    secured sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :facebook_access_token, :facebook_id, :location, :name, :password_digest
+  attr_accessible :email, :facebook_access_token, :facebook_id, :location, :name, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
 end
