aa5b45152909cfdeb38006fed1b39ae2afa90ee2
Raghu Betina <rbetina@gmail.com>
2013-06-05 08:04:21 -0500

    rails new, changed Gemfile to include bootstrap gem, bcrypt, and Heroku modifications.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = 'd5489a359fbcb222e6cd3f668da506ba161527d02f7cd40e2707eec5b78780d46a7b602f778df652be14122d90d27541093be7ad3584e7fa260d2b0c18ff64b9'
