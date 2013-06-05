58189be5090b8a7acb9652bcff828f31c669eff1
Raghu Betina <rbetina@gmail.com>
2013-06-05 11:10:53 -0500

    Starting to make the final exchange for access token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,15 @@
+require 'open-uri'
+
 class AuthController < ApplicationController
   def facebook
     @code = params[:code]
+
+    app_id = ""
+    redirect_uri = ""
+    app_secret = ""
+
+    url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{@code}"
+
+    @result = open(url).read
   end
 end
