2d4498160d509adef540644136429ca201770cf2
Raghu Betina <rbetina@gmail.com>
2013-06-05 11:42:26 -0500

    Completed OAuth handshake

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,16 +2,21 @@ require 'open-uri'
 
 class AuthController < ApplicationController
   def facebook
-    @code = params[:code]
+    code = params[:code]
 
     app_id = "1375153749364030"
     redirect_uri = "http://localhost:3000/auth/facebook"
     app_secret = "1dd681ba564eb102fd913583a4ed3678"
 
-    url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{@code}"
+    url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{code}"
 
-    @result = open(url).read
+    result = open(url).read
 
-    # Now, write some Ruby to extract from @result just the access token and store it in the database for the signed-in user (there is a column in the users table called 'facebook_access_token').
+    access_token = result.split("&").first.split("=").last
+
+    current_user.facebook_access_token = access_token
+    current_user.save
+
+    redirect_to current_user
   end
 end
