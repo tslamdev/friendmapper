51d94c8ce03b026331bacda4a3e6891a861b9555
Raghu Betina <rbetina@gmail.com>
2013-06-05 08:49:02 -0500

    cleaned up application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :signed_in?, :current_user
+
+  def signed_in?
+    session[:user_id].present?
+  end
+
+  def current_user
+    @user ||= User.find_by_id(session[:user_id])
+  end
 end
