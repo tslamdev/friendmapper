f254dad62d64287e67e55d4008cd09f4a72cd61c
Raghu Betina <rbetina@gmail.com>
2013-06-05 11:30:26 -0500

    Continuing final leg of handshake. Still need to parse and store access token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,12 +4,14 @@ class AuthController < ApplicationController
   def facebook
     @code = params[:code]
 
-    app_id = ""
-    redirect_uri = ""
-    app_secret = ""
+    app_id = "1375153749364030"
+    redirect_uri = "http://localhost:3000/auth/facebook"
+    app_secret = "1dd681ba564eb102fd913583a4ed3678"
 
     url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{@code}"
 
     @result = open(url).read
+
+    # Now, write some Ruby to extract from @result just the access token and store it in the database for the signed-in user (there is a column in the users table called 'facebook_access_token').
   end
 end
