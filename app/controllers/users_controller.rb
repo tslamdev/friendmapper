900dd45b65c36407d48d79e670d143885cbef684
Jeff Cohen <jeff@starterleague.com>
2013-06-07 09:25:06 -0500

    Add a user mailer for when they sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,8 @@
+# def dispatch
+#   x = UsersController.new
+#   x.create
+# end
+
 class UsersController < ApplicationController
   before_filter :require_authorized_user, only: [:show, :edit, :update, :destroy]
 
@@ -52,6 +57,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        email = UserMailer.welcome(@user)
+        email.deliver
+
         session[:user_id] = @user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome(user)
+    @greeting = "Hi!  We are sending this to #{user.email}."
+
+    mail to: "to@example.org"
+  end
+end
