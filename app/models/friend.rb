9d98781fe5ddc1b91d6b691541f3c4fa673a5f96
Raghu Betina <rbetina@gmail.com>
2013-06-10 13:29:58 -0500

    Added info window with friend's picture

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -11,4 +11,8 @@ class Friend < ActiveRecord::Base
   def gmaps4rails_address
     self.location
   end
+
+  def gmaps4rails_infowindow
+    "<img src='https://graph.facebook.com/#{self.facebook_id}/picture'> #{self.name}"
+  end
 end
